3b4a9b5d0b4afd235b67dc1c3ecafa37c8ecebeb
GuiFerrarezFerreira <gui.ferrarez.ferreira@gmail.com>
2025-06-21 12:33:28 -0300

    alterações

--- js/services/api.service.js
+++ js/services/api.service.js
@@ -4,7 +4,7 @@
 
 class ApiService {
     constructor() {
-        this.baseURL = 'http://localhost/trello-clone/api';
+        this.baseURL = 'http://localhost/trello_clone/api';
         this.token = localStorage.getItem('authToken');
     }
 
